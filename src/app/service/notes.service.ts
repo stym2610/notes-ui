110c27314ddaaef5309dff3db4af6a869e36b8d0
Satyam Tiwari <satyam.tiwari2610@gmail.com>
2020-06-30 19:10:34 +0530

    updated api url

--- src/app/service/notes.service.ts
+++ src/app/service/notes.service.ts
@@ -8,8 +8,8 @@ import { HttpClient } from '@angular/common/http';
 
 export class NotesService {
 
-  // private url = "https://notes--app-api.herokuapp.com/notes";
-  private url = "http://localhost:3000/notes";
+  private url = "https://notes--app-api.herokuapp.com/notes";
+  // private url = "http://localhost:3000/notes";
 
   constructor(private http: HttpClient) { }
 
